d203b3ff4bccbe69a23d9e3d7032815d16b56830
Dell <Dell@DESKTOP-9KJDALI>
2022-04-29 21:34:37 +0530

    First Commit

---
+++ GitHubDemo1/src/test/java/com/testcases/LoginTest.java
@@ -0,0 +1,10 @@
+package com.testcases;
+
+public class LoginTest {
+	
+	public void loginTest() {
+		System.out.println("This is login Test");
+		System.out.println("This is my first test");
+	}
+
+}
